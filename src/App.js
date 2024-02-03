7d6cea28ab0a057e4aea54ac158dd1f5c1aa2c74
sinha-rakshit <sinha.rakshit.16@gmail.com>
2024-02-04 00:16:42 +0530

    learnt components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
 
+const Person = () => {
+  return (
+    <>
+      <h1>First Name: Rakshit</h1>
+      <h2>Last Name: Sinha</h2>
+      <h2>Age: 18</h2>
+    </>
+  );
+};
+
 const App = () => {
   return (
     <div className="App">
-      <h1>Hello, JSM!</h1>
+      <Person />
     </div>
   );
 };
