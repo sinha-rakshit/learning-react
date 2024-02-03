d44ebb37403a4174a81aa344490ea9a48c9ff4ef
sinha-rakshit <sinha.rakshit.16@gmail.com>
2024-02-04 00:22:54 +0530

    learnt props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 
-const Person = () => {
+const Person = (props) => {
   return (
     <>
-      <h1>First Name: Rakshit</h1>
-      <h2>Last Name: Sinha</h2>
-      <h2>Age: 18</h2>
+      <h1>First Name: {props.FirstName}</h1>
+      <h2>Last Name: {props.LastName}</h2>
+      <h2>Age: {props.Age}</h2>
     </>
   );
 };
@@ -13,7 +13,8 @@ const Person = () => {
 const App = () => {
   return (
     <div className="App">
-      <Person />
+      <Person FirstName={"Rakshit"} LastName={"Sinha"} Age={21} />
+      <Person FirstName={"Yash"} LastName={"Raj"} Age={19} />
     </div>
   );
 };
