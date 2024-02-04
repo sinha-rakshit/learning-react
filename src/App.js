2990a29c8c88a3e90ae0b4bb6c6af55aba5403ca
sinha-rakshit <sinha.rakshit.16@gmail.com>
2024-02-05 00:58:39 +0530

    imported API and wrote CSS and svg

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import { useEffect } from "react";
+
+import "./App.css";
+import SearchIcon from "./search.svg";
+
+const API_URL = "http://www.omdbapi.com?apikey=5c13dbf3";
 
 const App = () => {
-  return <h1>App</h1>;
+  const SearchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    SearchMovies("SpiderMan");
+  }, []);
+  return (
+    <div className="app">
+      <h1>MovieHub</h1>
+    </div>
+  );
 };
 
 export default App;
