d05e6fdcfc4016737f4850b9ef99ec541f07842b
sinha-rakshit <sinha.rakshit.16@gmail.com>
2024-02-05 01:37:22 +0530

    made suitable changes in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 import "./App.css";
 import SearchIcon from "./search.svg";
 
+import MovieCard from "./MovieCard.jsx";
+
 const API_URL = "http://www.omdbapi.com?apikey=5c13dbf3";
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
   const SearchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data.Search);
+    setMovies(data.Search);
   };
 
   useEffect(() => {
-    SearchMovies("SpiderMan");
+    SearchMovies("Animal");
   }, []);
   return (
     <div className="app">
-      <h1>MovieHub </h1>
+      <h1>MovieHub</h1>
+
       <div className="search">
         <input
           placeholder="Search For Movies"
@@ -26,6 +30,18 @@ const App = () => {
         />
         <img src={SearchIcon} alt="search" onClick={() => {}} />
       </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No Movies Found</h2>
+        </div>
+      )}
     </div>
   );
 };
